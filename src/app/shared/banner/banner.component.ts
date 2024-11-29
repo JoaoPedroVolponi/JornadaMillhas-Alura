a26f51cbf3480bf0feaeb0b059edebb25b25d06f
João Pedro Volponi <jpvrosada@hotmail.com>
2024-11-29 16:25:17 -0300

    Banner component

--- src/app/shared/banner/banner.component.ts
+++ src/app/shared/banner/banner.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss']
 })
-export class BannerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class BannerComponent {
+  @Input() src: string = ' '
+  @Input() alt: string = ' '
 }
