8d6a19e050806d530c28c0ca3592b28dfd3f6081
João Pedro Volponi <jpvrosada@hotmail.com>
2024-12-06 17:05:27 -0300

    Lint - banner.components / spec  ts

--- src/app/shared/banner/banner.component.ts
+++ src/app/shared/banner/banner.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-banner',
-  templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  selector: "app-banner",
+  templateUrl: "./banner.component.html",
+  styleUrls: ["./banner.component.scss"],
 })
 export class BannerComponent {
-  @Input() src: string = ' '
-  @Input() alt: string = ' '
+  @Input() src: string = " ";
+  @Input() alt: string = " ";
 }
